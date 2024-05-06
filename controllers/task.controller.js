9bb2e91271602e3440e59fea3a1d171c6da96422
Harry EMARH <emarh.harry.code@gmail.com>
2024-05-07 00:13:50 +0200

    Get a Single Task

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,26 +1,41 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 const Task = require("../models/task.model");
 
 /********** Create a new Task ***********/
 const create = asyncHandler(async (req, res) => {
-    try {
-      const task = await Task.create(req.body);
-      res.status(200).json(task);
-    } catch (error) {
-      res.send(500).json({
-        message: error.message,
-      });
-    }
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.send(500).json({
+      message: error.message,
+    });
+  }
 });
 
 /********** Get All Tasks ***********/
 const getAll = asyncHandler(async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.status(200).json(tasks);
-    } catch(error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-module.exports = { create, getAll };
+/********** Get A Task ***********/
+const getSingleTask = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+
+    if (!task) {
+      return res.status(404).json(`No task with id: ${id}`);
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = { create, getAll, getSingleTask };
--- routes/task.route.js
+++ routes/task.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { create, getAll } = require("../controllers/task.controller");
+const { create, getAll, getSingleTask } = require("../controllers/task.controller");
 
 const taskRouter = express.Router();
 
 taskRouter.post("/", create);
 taskRouter.get("/", getAll);
+taskRouter.get("/:id", getSingleTask);
 
 module.exports = taskRouter;
