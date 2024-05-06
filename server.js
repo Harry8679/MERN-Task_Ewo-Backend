675fafa5faad4f3003a9d3f7b3f937a5cb56193d
Harry EMARH <emarh.harry.code@gmail.com>
2024-05-06 18:25:04 +0200

    Refactor Connection Sequence

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const dotenv = require('dotenv');
-const connectDB = require('./config/connectDB');
+const dotenv = require("dotenv");
+const connectDB = require("./config/connectDB");
 dotenv.config();
 
-connectDB();
-
 const port = process.env.PORT;
-console.log(process.env.PORT);
+// console.log(process.env.PORT);
 
-app.listen(port, () => {
-    console.log(`Server is running on the port ${port}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on the port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
